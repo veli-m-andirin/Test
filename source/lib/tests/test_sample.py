f92ed5b2985b02409f39adf7ef11c132df0ed470
Veli Murat ANDIRIN <62054739+veli-m-andirin@users.noreply.github.com>
2022-04-21 06:37:28 -0400

    #1 adds failing unit test

---
+++ source/lib/tests/test_sample.py
@@ -0,0 +1,10 @@
+from unittest import main, TestCase
+
+class Test(TestCase):
+    def test_basic(self):
+        
+        self.assertTrue(2 == 3)
+        return
+
+if __name__ == '__main__':
+    main()
